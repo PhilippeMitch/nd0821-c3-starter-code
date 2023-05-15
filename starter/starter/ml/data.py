75f8c570e6030dc40c1ad64b70cb23c81eee98b5
PhilippeMitch <philippemitch91@gmail.com>
2023-05-15 10:11:37 +0700

    Done with train_model, api  and  the tests

--- starter/starter/ml/data.py
+++ starter/starter/ml/data.py
@@ -54,14 +54,14 @@ def process_data(
     X_continuous = X.drop(*[categorical_features], axis=1)
 
     if training is True:
-        encoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
+        encoder = OneHotEncoder(sparse_output=False, handle_unknown="ignore")
         lb = LabelBinarizer()
         X_categorical = encoder.fit_transform(X_categorical)
         y = lb.fit_transform(y.values).ravel()
     else:
         X_categorical = encoder.transform(X_categorical)
         try:
-            y = lb.transform(y.values).ravel()
+            y = lb.fit_transform(y.values).ravel()
         # Catch the case where y is None because we're doing inference.
         except AttributeError:
             pass
